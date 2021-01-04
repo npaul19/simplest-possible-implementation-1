6828e4567cc7632b1d02fce9c7a695a96a2e2d43
npaul19 <nishtha.paul@thoughtworks.com>
2021-01-04 22:10:16 +0530

    Test1 Implementation

---
+++ src/main/java/simplest/possible/implementation/PrimeFactors.java
@@ -0,0 +1,11 @@
+package simplest.possible.implementation;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+public class PrimeFactors {
+    public List<Integer> of(int n) {
+        return Lists.newArrayList();
+    }
+}
